d425fe1dd95fad2453413bdedf3fc088874b168a
Simon Davis <simon_freelance@hotmail.com>
2019-02-22 11:32:50 -0500

    GET route to get user by ID completed.

--- src/main/java/com/lambdaschool/todos/controllers/UserController.java
+++ src/main/java/com/lambdaschool/todos/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.lambdaschool.todos.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,15 @@ public class UserController {
     @GetMapping("")
     public List<User> allUsers() {
         return userRepo.findAll();
+    }
+
+    @GetMapping("userid/{userid}")
+    public User getUserById(@PathVariable long userid) {
+        var user = userRepo.findById(userid);
+
+        if(user.isPresent()) {
+            return user.get();
+        }
+        return null;
     }
 }
