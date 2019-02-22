d9e1d2986ad9fa697913c8aad5c3d4555c773547
Simon Davis <simon_freelance@hotmail.com>
2019-02-22 11:58:54 -0500

    POST route to create new user implemented.

--- src/main/java/com/lambdaschool/todos/controllers/UserController.java
+++ src/main/java/com/lambdaschool/todos/controllers/UserController.java
@@ -2,13 +2,14 @@ package com.lambdaschool.todos.controllers;
 
 import com.lambdaschool.todos.models.User;
 import com.lambdaschool.todos.repositories.UserRepository;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
@@ -25,6 +26,14 @@ public class UserController {
 //    PUT /users/userid/{userid} - updates a user based on userid
 //    DELETE /users/userid/{userid} - Deletes a user based off of their userid and deletes all their associated todos
 
+    @ApiOperation(value = "List All Users", response = List.class)
+    @ApiResponses(value =
+            {
+                    @ApiResponse(code = 200, message = "Successfully retrieves list"),
+                    @ApiResponse(code = 401, message = "You are not authorized to the view the resource"),
+                    @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+                    @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+            })
     @GetMapping("")
     public List<User> allUsers() {
         return userRepo.findAll();
@@ -44,5 +53,11 @@ public class UserController {
     public User getUserByName(@PathVariable String username) {
         return userRepo.findByName(username);
     }
+
+    @PostMapping("")
+    public User addNewUser(@RequestBody User user) throws URISyntaxException {
+        return userRepo.save(user);
+    }
+
 
 }
