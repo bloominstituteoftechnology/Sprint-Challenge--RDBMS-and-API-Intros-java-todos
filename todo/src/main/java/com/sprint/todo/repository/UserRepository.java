3d5880753f9198f084775b6eb733431fe08e3390
rushi444 <rushi44@gmail.com>
2019-07-19 09:29:02 -0700

    Creates Repos for User and Role

---
+++ todo/src/main/java/com/sprint/todo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.sprint.todo.repository;
+
+import com.sprint.todo.models.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long>
+{
+    User findByUsername(String username);
+}
