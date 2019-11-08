12fb3ff0eb8d128a286698db469f7ba76ff914b5
RonnieDipple <Ronniedipple@gmail.com>
2019-11-08 10:28:37 -0600

    Initialization
    Initialization of project
    
    Co-Authored-By: Brandon Lively <bl-coderovertime@users.noreply.github.com>

---
+++ src/main/java/com/lambdaschool/demo/SprintChallengeRdbmsAndApiIntrosJavaTodosApplication.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintChallengeRdbmsAndApiIntrosJavaTodosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintChallengeRdbmsAndApiIntrosJavaTodosApplication.class, args);
+    }
+
+}
