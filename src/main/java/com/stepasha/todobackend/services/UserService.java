1baabf70975cab255d5bc5be93712dadb5efeee0
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2019-11-08 10:28:03 -0700

    Create UserService.java
    working on just READing data

---
+++ src/main/java/com/stepasha/todobackend/services/UserService.java
@@ -0,0 +1,12 @@
+package com.stepasha.todobackend.services;
+
+import com.stepasha.todobackend.models.User;
+
+import java.util.List;
+
+public interface UserService {
+    
+    List<User> findAll();
+    User findUserById(long id);
+
+}
