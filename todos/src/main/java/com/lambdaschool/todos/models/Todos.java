eee914eb6c0b86f8ce50ad7b0ced85c74f862715
W-B28 <wtb2804@gmail.com>
2020-09-11 12:53:23 -0500

    commit before 1st deadline

--- todos/src/main/java/com/lambdaschool/todos/models/Todos.java
+++ todos/src/main/java/com/lambdaschool/todos/models/Todos.java
@@ -34,4 +34,36 @@ public class Todos extends Auditable
         this.user = user;
         this.description = description;
     }
+
+    public long getTodoid() {
+        return todoid;
+    }
+
+    public void setTodoid(long todoid) {
+        this.todoid = todoid;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
